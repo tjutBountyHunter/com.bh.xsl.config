4a4c9f095419d1c1fb117f791a14b7ad82c572a9
helinhong <tjut_hlh@qq.com>
2018-10-14 23:06:11 +0800

    统一配置管理中心

---
+++ xsl-config-web/src/main/java/com/example/xslconfigweb/XslConfigWebApplication.java
@@ -0,0 +1,14 @@
+package com.example.xslconfigweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XslConfigWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XslConfigWebApplication.class, args);
+        System.out.print("hello");
+        System.out.print("hello");
+    }
+}
---
+++ xsl-config-web/src/main/java/com/example/xslconfigweb/demo.java
@@ -0,0 +1,4 @@
+package com.example.xslconfigweb;
+
+public class demo {
+}
