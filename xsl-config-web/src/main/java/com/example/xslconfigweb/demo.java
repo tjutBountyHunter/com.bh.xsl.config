66e8355ccc33b7a6e9767cf0406043e372c5d051
helinhong <tjut_hlh@qq.com>
2018-10-15 00:46:14 +0800

    测试controller运行

--- xsl-config-web/src/main/java/com/example/xslconfigweb/demo.java
+++
@@ -1,4 +0,0 @@
-package com.example.xslconfigweb;
-
-public class demo {
-}
--- xsl-config-web/src/test/java/controller/XslConfigWebApplicationTests.java
+++ xsl-config-web/src/test/java/controller/XslConfigWebApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.xslconfigweb;
+package controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
